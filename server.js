ad408024fea7b7f2d583bf9c801bb5736ba5daaf
Jon Martin <jonathanmartinnyc@gmail.com>
2020-10-10 14:42:24 -0400

    db name changed

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const uri = 'mongodb+srv://user1:squat1234@cluster0.wfjgo.mongodb.net/mathercize
 
 
 
-var dodgeDB;
+var mathercizeDB;
 
 MongoClient.connect(uri, function (err, client) {
     if (err) throw err;
-    dodgeDB = client.db('mathercizedb').collection('mathercize_coll');
+    mathercizeDB = client.db('mathercizedb').collection('mathercize_coll');
 });
 app.use(express.static(__dirname));
 
@@ -48,7 +48,7 @@ io.sockets.on('connection',
         socket.on('register_user',
             function(data) {
                 var unique_username = data.unique_username;
-                dodgeDB.insertOne({
+                mathercizeDB.insertOne({
                     username: unique_username
                 }, function (err, result) {
                     if (err) throw err;
@@ -61,7 +61,7 @@ io.sockets.on('connection',
                 console.log('register score on server')
                 var unique_username = data.unique_username;
                 var score = data.score;
-                dodgeDB.insertOne({
+                mathercizeDB.insertOne({
                     username: unique_username,
                     score: score
                 }, function (err, result) {
@@ -72,7 +72,7 @@ io.sockets.on('connection',
 
 
         socket.on('get_scores', function(data) {
-            dodgeDB.find( {} ).sort( { score: -1 }).toArray(
+            mathercizeDB.find( {} ).sort( { score: -1 }).toArray(
                 function(err, result) {
 
                     //console.log(result);
